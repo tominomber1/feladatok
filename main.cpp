04ebe310e4a06c9b4410e915687d44cd5e461e65
tominomber1 <148947426+tominomber1@users.noreply.github.com>
2024-01-17 10:40:40 +0200

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -4,22 +4,28 @@ using namespace std;
 
 int main()
 {
-    int n,aux,szamjegy,ujszam;
-    cout<<"n=";
-    cin>>n;
-    ujszam=0;
-    aux=n;
-    while(aux>0)
-      {
-      szamjegy=aux%10;
-    aux=aux/10;
-    ujszam=ujszam*10+szamjegy;
+    int a,b,aux,ujszam;
+    cout<<"a=";
+    cin>>a;
+    cout<<"b=";
+    cin>>b;
+    for(int szam=a;szam<=b;szam++){
+      bool prim=true;
+      for(int oszto=2;oszto<=szam/2;oszto++){
+        if(szam%oszto==0){
+          prim=false;
+        }
+      }
+      aux=szam;
+      ujszam=0;
+    while(aux!=0){
+      int szj=aux%10;
+      aux/=10;
+      ujszam=ujszam*10+szj;
     }
-    if(n==ujszam)
-      {
-      cout<<"a szam pallindrom";}
-    else
-    {
-      cout<<"a szam nem pallindrom";}
+    if((ujszam==szam)&&(prim)){
+      cout<<szam<<endl;
+    }
+    }
     return 0;
 }
