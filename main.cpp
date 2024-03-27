7c1cbec1caa4b3d08ae288bdb98a36e2018fe863
tominomber1 <148947426+tominomber1@users.noreply.github.com>
2024-03-27 10:48:00 +0200

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -1,33 +1,34 @@
 #include <iostream>
-
+#include <stdlib.h>
 using namespace std;
 
 int main()
 {
+    int n=10,i,j,aux;
     int v[100];
-    int n,i;
-    cout<<"n=";
-    cin>>n;
     for(i=0;i<n;i++)
     {
-     cout<<"v["<<i<<"]=";
-     cin>>v[i];
+      v[i]=rand()%100;
     }
-    for(i=0;i<n-1;i++)
+    for(i=0;i<n;i++)
+    {
+       cout<<v[i]<<" ";
+    }
+    cout<< endl;
+    for (i=0; i< n-1; i++)
         {
-        for(int j=i+1;j<n;j++)
-        {
-            if(v[i]>v[j])
+            for (j=i+1; j< n; j++)
+            {         if ( v[i]> v[j])
                 {
-                int csere=v[i];
-                v[i]=v[j];
-                v[j]=csere;
+                    aux= v[j];
+                    v[j]= v[i];
+                    v[i]=aux;
+                }
             }
         }
-    }
-    for(i=0;i<n;i++){
-     cout<<v[i]<<" ";
-
-    }
+        for(i=0;i<n;i++)
+            {
+        cout<<v[i]<<" ";
+            }
     return 0;
 }
