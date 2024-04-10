009a2544bb4feab3656c3899746a74a02c5be52c
tominomber1 <148947426+tominomber1@users.noreply.github.com>
2024-04-10 10:47:34 +0300

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -1,34 +1,15 @@
 #include <iostream>
-#include <stdlib.h>
+#include <fstream>
+
 using namespace std;
 
-int main()
-{
-    int n=10,i,j,aux;
-    int v[100];
-    for(i=0;i<n;i++)
+int main(){
+
+    int a;
+    ifstream myfile ("input.txt");
+    while (myfile >> a)
     {
-      v[i]=rand()%100;
+        cout << "a beolvasott szam:" << a << endl;
     }
-    for(i=0;i<n;i++)
-    {
-       cout<<v[i]<<" ";
-    }
-    cout<< endl;
-    for (i=0; i< n-1; i++)
-        {
-            for (j=i+1; j< n; j++)
-            {         if ( v[i]> v[j])
-                {
-                    aux= v[j];
-                    v[j]= v[i];
-                    v[i]=aux;
-                }
-            }
-        }
-        for(i=0;i<n;i++)
-            {
-        cout<<v[i]<<" ";
-            }
     return 0;
 }
