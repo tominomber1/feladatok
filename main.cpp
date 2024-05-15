1f9902a486fd97dcc0722869644b22a744c96ece
tominomber1 <148947426+tominomber1@users.noreply.github.com>
2024-05-15 10:33:31 +0300

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -3,13 +3,18 @@
 
 using namespace std;
 
-int main(){
+int main()
+{
 
-    int a;
-    ifstream myfile ("input.txt");
-    while (myfile >> a)
-    {
-        cout << "a beolvasott szam:" << a << endl;
+    ifstream in("input.txt");
+    ofstream out("output.txt");
+    int n,v[20];
+    in>>n;
+    for(int i=0;i<n;i++){
+        in>>v[i];
+        out<<v[i]<<" "<<v[i]/2<<endl;
     }
+    out.close();
+    in.close();
     return 0;
 }
