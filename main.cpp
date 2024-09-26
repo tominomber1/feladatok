70c39008db6fddd9f687ebe4daa979f5735e1fd6
tominomber1 <148947426+tominomber1@users.noreply.github.com>
2024-09-26 10:38:38 +0300

    Add files via upload
    simetrikus matrix

--- main.cpp
+++ main.cpp
@@ -4,42 +4,34 @@ using namespace std;
 
 int main()
 {
-    int matrix[10][10],sor,oszlop,n,e,k,ny,d,S;
-    ifstream in("input.txt");
-    in>>n;
-    for(sor=0;sor<n;sor++){
-        for(oszlop=0;oszlop<n;oszlop++){
+int a[10][10],n,sor,oszlop;
+ifstream in("input.txt");
+in>>n;
+for(sor=0;sor<n;sor++){
+    for(oszlop=sor+1;oszlop<n;oszlop++){
+        in>>a[sor][oszlop];
+    }
+}
+for(sor=0;sor<n;sor++){
+    for(oszlop=0;oszlop<n;oszlop++){
+        in>>a[sor][oszlop];
 
-            in>>matrix[sor][oszlop];
-        }
+if(sor==oszlop){
+    a[sor][oszlop]=1;
+}
+if(sor+oszlop==n-1){
+    a[sor][oszlop]=1;
+}
+}
+}
+for(sor=0;sor<n;sor++){
+    for(oszlop=sor+1;oszlop<n;oszlop++){
+        in>>a[sor][oszlop];
     }
-    for(sor=0;sor<n;sor++){
-        for(oszlop=0;oszlop<n;oszlop++){
-            in>>matrix[sor][oszlop];
 
-    }
-    if(sor<oszlop){
-            {
-               if(sor+oszlop<n-1){
-                 e+=matrix[sor][oszlop];
-               }
-               if(sor+oszlop>n-1){
-                k+=matrix[sor][oszlop];
-               }
-            }
-    }
-    if(sor>oszlop){
-        if(sor+oszlop<n-1){
-            ny+=matrix[sor][oszlop];
-        }
-    if(sor+oszlop>n-1){
-        d+=matrix[sor][oszlop];
-    }
-    }
-    }
-    cout<<"E="<<e<<endl;
-    cout<<"K="<<k<<endl;
-    cout<<"NY="<<ny<<endl;
-    cout<<"D="<<d<<endl;
-    return 0;
+cout<<a[sor][oszlop]<<" ";
+}
+cout<<endl;
+in.close();
+return 0;
 }
