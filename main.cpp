9cbf6223acf1e0d55bc985b13957234f07479394
tominomber1 <148947426+tominomber1@users.noreply.github.com>
2024-10-11 07:47:00 +0300

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -4,27 +4,33 @@ using namespace std;
 
 int main()
 {
-int n,a[10][10],b[10][10],szorzat[10][10],sor,oszlop,S;
+int n,a[10][10],sor,oszlop;
 ifstream in("input.txt");
 in>>n;
 for(sor=0;sor<n;sor++){
     for(oszlop=0;oszlop<n;oszlop++){
         in>>a[sor][oszlop];
-        in>>b[sor][oszlop];
     }
 }
+sor=0;
+for(oszlop=0;oszlop<n;oszlop++){
+      cout<<a[0][oszlop];
+}
+cout<<endl;
+sor=n-1;
+for(oszlop=0;oszlop<n;oszlop++){
+    cout<<a[n-1][oszlop];
+}
+cout<<endl;
+oszlop=0;
 for(sor=0;sor<n;sor++){
-    for(oszlop=0;oszlop<n;oszlop++){
-            for(int index=0;index<n;index++){
-       szorzat[sor][oszlop]+=a[sor][index]*b[index][oszlop];
-    }
+    cout<<a[sor][0];
 }
-}
+cout<<endl;
+oszlop=n-1;
 for(sor=0;sor<n;sor++){
-    for(oszlop=0;oszlop<n;oszlop++){
-          cout<<szorzat[sor][oszlop]<<" ";
-    }
-    cout<<endl;
+    cout<<a[sor][n-1];
 }
-    return 0;
+cout<<endl;
+return 0;
 }
